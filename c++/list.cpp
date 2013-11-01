310416b607a33ba71871163816fa91b1e9299598
xcleung <xcleung0221@gmail.com>
2013-11-01 20:19:08 +0800

    更新链表操作

--- c++/list.cpp
+++ c++/list.cpp
@@ -19,10 +19,14 @@ void print(LNode *head);
 void printReverse(LNode *head);
 void addToTail(LNode **head,int value);
 void addToHead(LNode **head,int value);
+void addToIndex(LNode **head,int value,int index);
+void deleteNode(LNode **head,int value);
+LNode *reverseListDG(LNode **head,LNode *p);
+LNode *reverseListDD(LNode *head);
 
 int main(){
     int n,val;
-    LNode *head=NULL;
+    LNode *head=NULL,*rhead=NULL;
     cout<<"请输入数字n：";
     cin>>n;
     for(int i=0;i<n;i++){
@@ -30,10 +34,16 @@ int main(){
         addToTail(&head,val);
     }
     addToHead(&head,16);
-    addToHead(&head,24);
+    addToIndex(&head,24,3);
+    //deleteNode(&head,4);
+    cout<<"原来的序列：";
     print(head);
+    cout<<"逆序输出：";
     printReverse(head);
     cout<<endl;
+    reverseListDG(&rhead,head);
+    cout<<"链表逆序后的输出：";
+    print(rhead);
     return 0;
 }
 
@@ -82,4 +92,64 @@ void addToHead(LNode **head,int value){
     }
 }
 
+void addToIndex(LNode **head,int value,int index){
+    LNode *newNode=new LNode();
+    newNode->data=value;
+    newNode->next=NULL;
+    int count=1;
+    if(*head==NULL){
+        *head=newNode;
+    }else if(index==1){
+        newNode->next=*head;
+        *head=newNode;
+    }else{
+        LNode *pre=*head;
+        LNode *p=*head;
+        while(p!=NULL && count!=index){
+            pre=p;
+            p=p->next;
+            count++;
+        }
+        newNode->next=p;
+        pre->next=newNode;
+    }
+}
+
+void deleteNode(LNode **head,int value){
+    LNode *toDelete=NULL;
+    LNode *p;
+    if(*head==NULL){
+        cout<<"链表为空，无法删除"<<endl;
+        return ;
+    }else if((*head)->data==value){
+        toDelete=*head;
+        *head=(*head)->next;
+    }else{
+        p=*head;
+        while(p->next!=NULL && p->next->data!=value){
+            p=p->next;
+        }
+        if(p->next!=NULL && p->next->data==value){
+            toDelete=p->next;
+            p->next=p->next->next;
+        }
+    }
+    if(toDelete!=NULL){
+        delete toDelete;
+        toDelete=NULL;
+    }
+}
+
+LNode *reverseListDG(LNode **head,LNode *p){
+    //cout<<p->data<<endl;
+    if(p->next==NULL){
+        *head=p;
+        return p;
+    }
+        reverseListDG(head,p->next)->next=p;
+        p->next=NULL;
+        return p;
+    
+
+}
 
